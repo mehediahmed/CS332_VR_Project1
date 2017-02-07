948286f903093cb1e0147122d7de5bb1b86010c7
mehediahmed <mehediahmed@umass.edu>
2017-02-06 21:44:38 -0500

    Crystals increase intensity and range when used and rumble depending on the charge left

---
+++ Assets/_scripts/Crystal/Crystal_Rumble.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Crystal_Rumble : VRTK_InteractHaptics
+{
+    private Crystal_Interact interact;
+
+
+    public override void HapticsOnUse(VRTK_ControllerActions controllerActions)
+    {
+        base.HapticsOnUse(controllerActions);
+
+    }
+    // Use this for initialization
+    void Awake () {
+        interact= gameObject.GetComponent<Crystal_Interact>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        strengthOnUse = interact.charges / 100f;
+	}
+
+
+
+}
