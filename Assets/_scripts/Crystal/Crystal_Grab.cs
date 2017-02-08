b58129e96baa0f207f49feaafed9846aaa329d0e
mehediahmed <mehediahmed@umass.edu>
2017-02-08 10:56:46 -0500

    Crystals done

--- Assets/_scripts/Crystal/Crystal_Grab.cs
+++ Assets/_scripts/Crystal/Crystal_Grab.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Crystal_Grab : MonoBehaviour {
+using VRTK;
+public class Crystal_Grab : VRTK_InteractGrab {
 
 	// Use this for initialization
 	void Start () {
