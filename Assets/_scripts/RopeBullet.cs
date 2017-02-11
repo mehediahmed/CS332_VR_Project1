ff262cfe4ac6a24d7b24383b1d07f80c554e0234
Paul Calande <paul.luap09@gmail.com>
2017-02-11 17:14:58 -0500

    Added rope gun and rope bullet scripts (still need prefabs)

---
+++ Assets/_scripts/RopeBullet.cs
@@ -0,0 +1,35 @@
+﻿// Author(s): Paul Calande
+// Script for the bullet fired by the rope gun.
+
+using UnityEngine;
+
+public class RopeBullet : MonoBehaviour
+{
+    // Reference to the rope object.
+    public GameObject rope;
+
+    // Number of seconds remaining before being deactivated.
+    private float life;
+
+    private void Update()
+    {
+        // If the bullet's life runs out, disable it.
+        life -= Time.deltaTime;
+        if (life <= 0.0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // If this bullet collides with an object with the Environment tag...
+        if (collision.gameObject.tag == "Environment")
+        {
+            // Deactivate the bullet.
+            gameObject.SetActive(false);
+            // Activate the rope.
+            rope.SetActive(true);
+        }
+    }
+}
---
+++ Assets/_scripts/RopeGun.cs
@@ -0,0 +1,56 @@
+﻿// Author(s): Paul Calande
+// Script for a gun that shoots a rope bullet.
+
+using UnityEngine;
+using VRTK;
+
+public class RopeGun : VRTK_InteractableObject
+{
+    // Reference to the rope gun bullet prefab.
+    public GameObject bulletPrefab;
+    // The object from which the rope gun spawns.
+    public GameObject bulletSpawner;
+    // Speed of the bullet.
+    public float bulletSpeed;
+    // Lifetime of the bullet (in seconds).
+    // If the bullet goes this long without colliding with anything, it will be destroyed.
+    public float bulletLifetime;
+
+    // Reference to the bullet GameObject.
+    private GameObject bullet;
+    // The bullet's Rigidbody component.
+    private Rigidbody rb;
+
+    // Called when the use trigger is pressed on the held object.
+    public override void StartUsing(GameObject currentUsingObject)
+    {
+        base.StartUsing(currentUsingObject);
+        FireBullet();
+    }
+
+    private void Start()
+    {
+        bullet = Instantiate(bulletPrefab);
+        rb = bullet.GetComponent<Rigidbody>();
+        bullet.SetActive(false);
+    }
+
+    private void FireBullet()
+    {
+        // If a bullet already exists, don't fire a bullet.
+        // This makes sure that only one bullet exists at a time.
+        if (bullet.activeInHierarchy)
+        {
+            return;
+        }
+        // Move the bullet to the position of the bullet spawner. Rotation, too.
+        Transform bulletTransform = bullet.transform;
+        Transform bulletSpawnerTransform = bulletSpawner.transform;
+        bulletTransform.position = bulletSpawnerTransform.position;
+        bulletTransform.rotation = bulletSpawnerTransform.rotation;
+        // Activate the bullet.
+        bullet.SetActive(true);
+        // Send the bullet on its merry way!
+        rb.AddForce(-bullet.transform.forward * bulletSpeed);
+    }
+}
