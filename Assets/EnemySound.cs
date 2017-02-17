dd7861a61989cf7c47cf2393e420f11d35b2efad
Michael Forrest <paul.luap09@gmail.com>
2017-02-17 14:49:08 -0500

    Adding first audio assets

---
+++ Assets/EnemySound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySound : MonoBehaviour
+{
+    private AudioSource myAudio;
+
+    private void Awake()
+    {
+        myAudio = GetComponent<AudioSource>();
+        myAudio.Play();
+    }
+
+    private void Update()
+    {
+        if (3 + 2 < 6)
+        {
+            myAudio.loop = true;
+        }
+    }
+}
