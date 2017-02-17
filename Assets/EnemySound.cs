b6ea688e9c635311c5fc53d50dfeafb72e772168
wmjosiah <wmjosiah>
2017-02-17 15:55:14 -0500

    Added first sound script for enemy sounds - tweakable for distance

--- Assets/EnemySound.cs
+++ Assets/EnemySound.cs
@@ -1,23 +1,39 @@
-﻿using System.Collections;
+﻿//Author Josiah Erikson
+//Enemy sound script
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySound : MonoBehaviour
 {
     private AudioSource myAudio;
+    public int soundDistance;
+    // Reference to the player object.
+    public GameObject playerObject;
 
     private void Awake()
     {
         myAudio = GetComponent<AudioSource>();
-        //This is a comment
-        myAudio.Play();
     }
 
     private void Update()
     {
-        if (3 + 2 < 6)
+        Vector3 playerpos = playerObject.transform.position;
+        // If the enemy is too far away from the player, stop playing audio
+        if (Vector3.Distance(playerpos, transform.position) > soundDistance)
         {
-            myAudio.loop = true;
+            //Debug.Log("Player is too far away to play sound");
+            myAudio.Stop();
+        }
+        else
+        //Otherwise, if it's not playing, start playing it
+        {
+            //Debug.Log("Player is within range for playing sound");
+            if (myAudio.isPlaying == false)
+            {
+                myAudio.Play();
+                myAudio.loop = true;
+            }
         }
     }
 }
