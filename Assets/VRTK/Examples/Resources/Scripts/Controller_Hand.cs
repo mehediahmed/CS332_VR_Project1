37010991e30ccaf793ee69b7b6dc83c19ae138d0
BearSheriff <mehediahmed2@gmail.com>
2017-02-19 03:46:29 -0500

    placeholder watch and new ligting system with The Lab Renderer

--- Assets/VRTK/Examples/Resources/Scripts/Controller_Hand.cs
+++ Assets/VRTK/Examples/Resources/Scripts/Controller_Hand.cs
@@ -74,8 +74,6 @@
 
         private void Update()
         {
-            pointerFinger.localEulerAngles = new Vector3(targetPointerRotation, 0f, 0f);
-            gripFingers.localEulerAngles = new Vector3(targetGripRotation, 0f, 0f);
         }
     }
 }
