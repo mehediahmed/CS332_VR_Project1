570302e8d1786716a67272662c45b00f077f7f1c
wmjosiah <wjerikson@hampshire.edu>
2017-02-19 17:43:12 -0500

    More sounds and climbing sounds implemented

---
+++ Assets/RockClimbingSounds.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+
+public class RockClimbingSounds : MonoBehaviour {
+
+    private VRTK_InteractableObject rock;
+    private AudioSource source;
+    public AudioClip rockGrabbedSound;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Awake ()
+    {
+        rock = GetComponent<VRTK_InteractableObject>();
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnEnable ()
+    {
+        //Subscribe to the grabbed event
+         rock.InteractableObjectGrabbed += GrabRock;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //What to do when the grabbed event is triggered
+    void GrabRock(object thisrock, VRTK.InteractableObjectEventArgs somearguments)
+    {
+        source.PlayOneShot(rockGrabbedSound);
+    }
+}
