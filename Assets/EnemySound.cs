c6592022fb19e2917f8b4009b52fb8ca0442a739
wmjosiah <wjerikson@hampshire.edu>
2017-02-19 10:39:14 -0500

    Enemy Sound Script

--- Assets/EnemySound.cs
+++ Assets/EnemySound.cs
@@ -9,18 +9,35 @@ public class EnemySound : MonoBehaviour
     private AudioSource myAudio;
     public int soundDistance;
     // Reference to the player object.
-    public GameObject playerObject;
+    private GameObject player;
 
     private void Awake()
     {
         myAudio = GetComponent<AudioSource>();
+        player = GameObject.FindWithTag("MainCamera");
+        Vector3 playerpos = player.transform.position;
+        //Debug.Log("Playerpos is " + playerpos);
+        //Debug.Log("Transform position is " + transform.position);
+        //Debug.Log("SoundDistance is " + soundDistance);
+        //Debug.Log("Distance between them is " + Vector3.Distance(playerpos, transform.position)); 
+        // If the enemy is too far away from the player, stop playing audio
+        if (Vector3.Distance(playerpos, transform.position) > soundDistance)
+        {
+          //  Debug.Log("Player is too far away to play sound");
+            myAudio.Stop();
+        }
     }
 
     private void Update()
     {
-        Vector3 playerpos = playerObject.transform.position;
+        Vector3 playerpos = player.transform.position;
         // If the enemy is too far away from the player, stop playing audio
-        if (Vector3.Distance(playerpos, transform.position) > soundDistance)
+        float playerDistance = Vector3.Distance(playerpos, transform.position);
+        //Debug.Log("Playerpos is " + playerpos);
+        //Debug.Log("Transform position is " + transform.position);
+        //Debug.Log("SoundDistance is " + soundDistance);
+        //Debug.Log("Distance between them is " + playerDistance);
+        if (playerDistance > soundDistance)
         {
             //Debug.Log("Player is too far away to play sound");
             myAudio.Stop();
@@ -28,7 +45,7 @@ public class EnemySound : MonoBehaviour
         else
         //Otherwise, if it's not playing, start playing it
         {
-            //Debug.Log("Player is within range for playing sound");
+           // Debug.Log("Player is within range for playing sound");
             if (myAudio.isPlaying == false)
             {
                 myAudio.Play();
