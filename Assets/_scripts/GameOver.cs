212cd570df1c8a82e9eb5e80413afd2e2339a975
ByteNybbler <paul.luap09@gmail.com>
2017-02-24 22:24:29 -0500

    GameOver scene now automatically boots the player back to the level they were just in after 5 seconds

---
+++ Assets/_scripts/GameOver.cs
@@ -0,0 +1,27 @@
+﻿// Author(s): Paul Calande
+// Game over scene script.
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(RespawnTimer());
+    }
+
+    IEnumerator RespawnTimer()
+    {
+        yield return new WaitForSeconds(5.0f);
+        ReturnToGame();
+    }
+
+    // Return to the last scene that the player was in before they died.
+    private void ReturnToGame()
+    {
+        string sceneName = Player.GetLastSceneBeforeDeath();
+        SceneManager.LoadScene(sceneName);
+    }
+}
