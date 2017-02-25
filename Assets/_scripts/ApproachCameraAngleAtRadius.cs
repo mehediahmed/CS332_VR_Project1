8dc7e3cb7e72c29de607f7d2b0fb4438862f4cb4
ByteNybbler <paul.luap09@gmail.com>
2017-02-24 22:48:37 -0500

    started work on weird movey camera followy menu thing

---
+++ Assets/_scripts/ApproachCameraAngleAtRadius.cs
@@ -0,0 +1,20 @@
+﻿// Author(s): Paul Calande
+// Makes the attached object move slowly towards where the main camera is looking.
+// The attached object stays a certain radius away from the main camera's position.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApproachCameraAngleAtRadius : MonoBehaviour
+{
+    // How far away the object stays from the main camera's position.
+    public float radius;
+
+    private void Update()
+    {
+        Vector3 cameraDirection = Camera.main.transform.eulerAngles;
+
+        //transform.position =
+    }
+}
